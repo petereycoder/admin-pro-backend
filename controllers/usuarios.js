3d2d6695d32fce66742019e44b2ee176889e484c
petereycoder <fp.pipe.reyes@gmail.com>
2024-09-20 10:33:51 -0600

    POST - Crear usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,12 +1,30 @@
+const Usuario = require('../models/usuarios');
+
 const getUsuarios = (req, res) => {
 
     res.json({
         ok: true,
-        usuarios: []
+        msg: 'get Usuarios'
+    });
+    
+}
+
+const crearUsuario = async(req, res) => {
+
+    const { email, password, nombre } = req.body;
+
+    const usuario = new Usuario( req.body );
+
+    await usuario.save();
+
+    res.json({
+        ok: true,
+        usuario
     });
     
 }
 
 module.exports = {
     getUsuarios,
+    crearUsuario,
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,10 +3,12 @@
 */
 
 const { Router } = require('express');
-const { getUsuarios } = require('../controllers/usuarios');
+const { getUsuarios, crearUsuario } = require('../controllers/usuarios');
 
 const router = Router();
 
 router.get('/', getUsuarios );
+
+router.post('/', crearUsuario );
 
 module.exports = router;
