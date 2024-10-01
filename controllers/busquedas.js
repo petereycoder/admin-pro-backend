35130ff02c210e2a66c43adc40d66deb4b7ed70d
petereycoder <fp.pipe.reyes@gmail.com>
2024-10-01 10:50:31 -0600

    Búsqueda por colección específica

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -23,6 +23,45 @@ const getTodo = async(req, res = response)  => {
     })
 }
 
+const getDocumentosColeccion = async(req, res = response)  => {
+
+    const tabla = req.params.tabla;
+    const busqueda = req.params.busqueda;
+    const regex = new RegExp( busqueda, 'i' );
+
+    let data = [];
+
+    switch( tabla ) {
+        case 'medicos':
+            data = await Medico.find({ nombre: regex })
+                         .populate('usuario', 'nombre img')
+                         .populate('hospital', 'nombre img');
+        break;
+
+        case 'hospitales':
+            data = await Hospital.find({ nombre: regex })
+                           .populate('usuario', 'nombre img');
+        break;
+
+        case 'usuarios':
+            data = await Usuario.find({ nombre: regex });
+        break;
+
+        default:
+            return res.status(400).json({
+                ok: false,
+                msg: 'La tabla tiene que ser usuarios/medicos/hospitales'
+            })
+
+    }
+
+    res.json({
+        ok: true,
+        resultados: data
+    })
+}
+
 module.exports = {
-    getTodo
+    getTodo,
+    getDocumentosColeccion
 }
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -1,14 +1,16 @@
 /*
-    ruta: api/todo/:busqueda
+    ruta: api/todo
 */
 
 const { Router } = require('express');
 
-const { getTodo } = require('../controllers/busquedas')
+const { getTodo, getDocumentosColeccion } = require('../controllers/busquedas')
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get('/:busqueda', validarJWT, getTodo );
+
+router.get('/:coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion );
 
 module.exports = router;
